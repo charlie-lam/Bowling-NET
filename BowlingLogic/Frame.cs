a6143ada5a3962143f1d874ca57b902a47444a9d
Charlie Lambert <93406456+charlie-lam@users.noreply.github.com>
2023-09-30 17:43:14 +0100

    Add frame class

---
+++ BowlingLogic/Frame.cs
@@ -0,0 +1,13 @@
+namespace BowlingLogic;
+
+public class Frame
+{
+    public int rollOne;
+    public int rollTwo;
+    public int rollThree;
+
+    public void AddRole(int pins, int roll)
+    {
+
+    }
+}
