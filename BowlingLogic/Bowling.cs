79c07c35122876b9ab220a1d8a9d94a4f9c6beba
Charlie Lambert <93406456+charlie-lam@users.noreply.github.com>
2023-09-30 17:40:33 +0100

    Start bowling class

--- BowlingLogic/Bowling.cs
+++ BowlingLogic/Bowling.cs
@@ -1,1 +1,13 @@
-﻿
+﻿namespace BowlingLogic;
+
+public class Bowling
+{
+    public bool HasStarted false;
+    public bool HasEnded false;
+    public int currentFrame = 0;
+
+    public void TakeRoll(int pins, int roll)
+    {
+        
+    }
+}
