9787bb4678ca6b3cf8749ab2d3ec68fbfbbb0420
Charlie Lambert <93406456+charlie-lam@users.noreply.github.com>
2023-10-01 23:50:48 +0100

    Update frame rollThree setting logic

--- BowlingLogic/Frame.cs
+++ BowlingLogic/Frame.cs
@@ -2,13 +2,30 @@ namespace BowlingLogic;
 
 public class Frame
 {
+    private int frameNumber;
+
+    public Frame(int frameNumber)
+    {
+        this.frameNumber = frameNumber;
+    }
     private int rollOne;
     private int rollTwo;
     private int rollThree;
 
     public int RollOne { get => rollOne; set => AddRole(ref rollOne, pins); }
     public int RollTwo { get => rollTwo; set => AddRole(ref rollTwo, pins); }
-    public int RollThree { get => rollThree; set => AddRole(ref rollThree, pins); }
+    public int RollThree 
+    { 
+        get => rollThree; 
+        set => 
+            {
+                if(frameNumber != 10 || (RollOne + RollTwo < 10))
+                {
+                    throw new ArgumentException("Third roll only available on the 10th frame and if previous two rolls are greater than 10.");
+                }
+                AddRole(ref rollThree, pins);
+            } 
+    }
     
 
     public void AddRole(ref roll, int pins)
