68af2ed57e9744b636a7d8325bc675a6d5124389
Charlie Lambert <93406456+charlie-lam@users.noreply.github.com>
2023-10-01 23:29:04 +0100

    Update frame logic

--- BowlingLogic/Frame.cs
+++ BowlingLogic/Frame.cs
@@ -2,12 +2,21 @@ namespace BowlingLogic;
 
 public class Frame
 {
-    public int rollOne;
-    public int rollTwo;
-    public int rollThree;
+    private int rollOne;
+    private int rollTwo;
+    private int rollThree;
 
-    public void AddRole(int pins, int roll)
+    public int RollOne { get => rollOne; set => AddRole(ref rollOne, pins); }
+    public int RollTwo { get => rollTwo; set => AddRole(ref rollTwo, pins); }
+    public int RollThree { get => rollThree; set => AddRole(ref rollThree, pins); }
+    
+
+    public void AddRole(ref roll, int pins)
     {
-
+        if(pins < 0 || pins > 10)
+        {
+            throw new ArgumentException("Pin value out of range");
+        }
+        return pins;
     }
 }
