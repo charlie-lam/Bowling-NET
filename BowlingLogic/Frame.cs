2abc3881bdd5f85fc4a6654c7ff8f182e0a9a3e6
Charlie Lambert <93406456+charlie-lam@users.noreply.github.com>
2023-10-02 23:30:43 +0100

    Fix frame methods

--- BowlingLogic/Frame.cs
+++ BowlingLogic/Frame.cs
@@ -12,25 +12,25 @@ public class Frame
     private int rollTwo;
     private int rollThree;
 
-    public int RollOne { get => rollOne; set => AddRole(ref rollOne, pins); }
-    public int RollTwo { get => rollTwo; set => AddRole(ref rollTwo, pins); }
-    public int RollThree 
-    { 
-        get => rollThree; 
-        set => 
-            {
-                if(frameNumber != 10 || (RollOne + RollTwo < 10))
-                {
-                    throw new ArgumentException("Third roll only available on the 10th frame and if previous two rolls are greater than 10.");
-                }
-                AddRole(ref rollThree, pins);
-            } 
-    }
-    
-
-    public void AddRole(ref roll, int pins)
+    public int RollOne { get => rollOne; set => AddRoll(ref rollOne, value); }
+    public int RollTwo { get => rollTwo; set => AddRoll(ref rollTwo, value); }
+    public int RollThree
     {
-        if(pins < 0 || pins > 10)
+        get => rollThree;
+        set
+        {
+            if (frameNumber != 10 || (RollOne + RollTwo < 10))
+            {
+                throw new ArgumentException("Third roll only available on the 10th frame and if previous two rolls are greater than 10.");
+            }
+            AddRoll(ref rollThree, value);
+        }
+    }
+
+
+    public int AddRoll(ref int roll, int pins)
+    {
+        if (pins < 0 || pins > 10)
         {
             throw new ArgumentException("Pin value out of range");
         }
