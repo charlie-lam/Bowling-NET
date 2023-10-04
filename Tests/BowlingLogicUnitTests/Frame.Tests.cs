8ee7739c48313de5630a8311674e355c39e2ac1c
Charlie Lambert <93406456+charlie-lam@users.noreply.github.com>
2023-10-04 23:41:44 +0100

    Wrtie first Frame set up tests

--- Tests/BowlingLogicUnitTests/Frame.Tests.cs
+++ Tests/BowlingLogicUnitTests/Frame.Tests.cs
@@ -1,10 +1,22 @@
 namespace Tests;
+using BowlingLogic;
 
 public class FrameTests
 {
     [Fact]
-    public void Test1()
+    public void Frame_InitialisedWithFrameNumber()
     {
+        //Arrange & Act
+        var frame = new Frame(1);
 
+        //Assert
+        frame.frameNumber.Should().Be(1);
+    }
+
+    [Fact]
+    public void Frame_InitialisedWithNull_ThrowsArgumentNullException()
+    {
+        //Act & Assert
+        Assert.Throws<ArgumentNullException>(()=> new Frame(null));
     }
 }
