67c15ab806514588c913d1646145724ebf038bdb
Charlie Lambert <93406456+charlie-lam@users.noreply.github.com>
2023-11-03 00:44:27 +0000

    Add reference in BowlingCli/ 1st iteration program

--- BowlingCLI/Program.cs
+++ BowlingCLI/Program.cs
@@ -1,2 +1,58 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using BowlingLogic;
+
+var frameFactory = new FrameFactory();
+var scoreboard = new Scoreboard();
+Bowling? bowlingGame = null;
+
+Console.WriteLine("Do you want to start a game of bowling: Yes or No");
+string? startGameResponse = null;
+while (string.IsNullOrWhiteSpace(startGameResponse))
+{
+    startGameResponse = Console.ReadLine();
+}
+if (startGameResponse.ToLower() == "yes")
+{
+    bowlingGame = new Bowling(scoreboard, frameFactory);
+    bowlingGame.StartGame();
+    while (!bowlingGame.HasEnded)
+    {
+        Console.WriteLine("Type your roll from 1 to 10:");
+        string? pinInput = Console.ReadLine();
+        Console.WriteLine();
+        if (int.TryParse(pinInput, out int pins) && pins >= 0 && pins <= 10)
+        {
+            var frames = bowlingGame.Scoreboard.Frames;
+            bowlingGame.TakeRoll(pins);
+            foreach (var frame in frames)
+            {
+                Console.WriteLine($"Frame {frame.FrameNumber}:");
+
+                if (frame.RollOne.HasValue)
+                {
+                    Console.WriteLine($"  Roll One: {frame.RollOne}");
+                }
+
+                if (frame.RollTwo.HasValue)
+                {
+                    Console.WriteLine($"  Roll Two: {frame.RollTwo}");
+                }
+
+                // Display Roll Three (if applicable, e.g., for the 10th frame)
+                if (frame.FrameNumber == 10 && frame.RollThree.HasValue)
+                {
+                    Console.WriteLine($"  Roll Three: {frame.RollThree}");
+                }
+
+                Console.WriteLine();
+            }
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Please enter a number from 0 to 10.");
+        }
+    }
+}
+else
+{
+    Console.WriteLine("Shame");
+}
