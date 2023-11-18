ee4354704b979009772258c3f0fdcd12b525e546
Charlie Lambert <93406456+charlie-lam@users.noreply.github.com>
2023-11-18 10:59:26 +0000

    Extract Strike and spare logic into methods

--- BowlingLogic/Scoreboard.cs
+++ BowlingLogic/Scoreboard.cs
@@ -27,21 +27,36 @@ public class Scoreboard : IScoreboard
         var twoFramesBefore = Frames.ElementAtOrDefault(frame.FrameNumber - 3);
         var oneFrameBefore = Frames.ElementAtOrDefault(frame.FrameNumber - 2);
 
-        if(twoFramesBefore != null && twoFramesBefore.RollOne == 10 && oneFrameBefore!.RollOne == 10)
+        if (TwoFramesBeforeIsAStrike(twoFramesBefore, oneFrameBefore))
         {
             UpdateBonusScore(frame.RollOne ?? 0, twoFramesBefore.FrameNumber);
         }
-        if(oneFrameBefore != null && oneFrameBefore.RollOne == 10)
+        if (PreviousFrameIsAStrike(oneFrameBefore))
         {
             var bonus = (frame.RollOne ?? 0) + (frame.RollTwo ?? 0);
             UpdateBonusScore(bonus, oneFrameBefore.FrameNumber);
         }
-        if(oneFrameBefore != null && oneFrameBefore.RollOne + oneFrameBefore.RollTwo == 10 && oneFrameBefore.RollTwo != 0)
+        if (PreviousFrameIsASpare(oneFrameBefore))
         {
             UpdateBonusScore(frame.RollOne ?? 0, oneFrameBefore.FrameNumber);
         }
     }
-    
+
+    private static bool PreviousFrameIsASpare(IFrame oneFrameBefore)
+    {
+        return oneFrameBefore != null && oneFrameBefore.RollOne + oneFrameBefore.RollTwo == 10 && oneFrameBefore.RollTwo != 0;
+    }
+
+    private static bool PreviousFrameIsAStrike(IFrame oneFrameBefore)
+    {
+        return oneFrameBefore != null && oneFrameBefore.RollOne == 10;
+    }
+
+    private static bool TwoFramesBeforeIsAStrike(IFrame? twoFramesBefore, IFrame oneFrameBefore)
+    {
+        return twoFramesBefore != null && twoFramesBefore.RollOne == 10 && oneFrameBefore!.RollOne == 10;
+    }
+
     public void AddBonusScore()
     {
         if(CanAddToList(_bonusScores.Count)) _bonusScores.Add(0);
